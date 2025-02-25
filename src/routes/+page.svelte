dd639672e6d754ff78d326eb42e511c7bb9ecacf
Apollo <apollomacabodbod8@gmail.com>
2025-02-25 15:05:58 +0800

    added test component in lib

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
+
+
+<script>
+
+ import Test from '$lib/Test.svelte'
+
+</script>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <a href="/about">About us</a>
+
+
+<Test/>
