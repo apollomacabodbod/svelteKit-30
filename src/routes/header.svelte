cada67be60acf5ade49a24106e57933af18d34fd
Apollo <apollomacabodbod8@gmail.com>
2025-02-25 14:24:17 +0800

    added styles in header

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,3 +4,20 @@
     <h1>Level Up Blog</h1>
 
 </header>
+
+<style>
+
+
+header{
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+}
+
+    h1 {
+        color: orange;
+    }
+
+
+
+</style>
