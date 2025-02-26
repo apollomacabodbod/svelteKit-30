07907d68a16eb105f9add4c74ad9eeaf2b31390d
Apollo <apollomacabodbod8@gmail.com>
2025-02-26 10:00:21 +0800

    display each item

---
+++ src/routes/show/[num]/+page.js
@@ -0,0 +1,17 @@
+
+
+
+export async function load({ fetch, params }){
+
+    const res = await fetch(`https://syntax.fm/api/shows/${params.num}`)
+    const data = await res.json();
+
+    console.log(data)
+
+    return {
+
+        episode: data
+    };
+
+
+}
